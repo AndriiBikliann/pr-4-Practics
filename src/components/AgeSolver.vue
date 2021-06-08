b965708f172db41316945e4fc9d7a39fc2463322
Andrii Bikliann <staremi100@gmail.com>
2021-06-08 20:58:24 +0300

    Innit commit

---
+++ src/components/AgeSolver.vue
@@ -0,0 +1,31 @@
+<!--<template>
+    <div>
+       <span v-if="isPensioner"> Турція відпочинок</span>
+       <!--<span v-else="isPensioner"> Бери лопату</span>-->
+    </div>
+</template>-->
+
+<script>  /*
+    export default {
+        name :'AgeSolver',
+        props: {
+            userAge: {
+                type: Number,
+                default: 0,
+            },
+            pensionerAge: {
+                type: Number,
+                default: 65,
+            },
+        },
+        computed : {
+            isPensioner() {
+                return this.userAge>this.pensionerAge
+            }
+        },
+    }  */
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/LabelComp.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="label-conteiner">
+       <div class="label-title">{{title}}</div>
+       <div class="label-numb">{{numbers}}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name : 'LabelComp',
+        props: {
+            title: {
+                type: String,
+                default: 'no title'
+            },
+            numbers: {
+                type: Number,
+                default: 1
+            }
+        }
+    }
+</script>
+
+<style lang="css">
+.label-conteiner {
+    width: 100px;
+    display: inline-flex;
+}
+.label-numb {
+    width: 20px;
+    background-color: black;
+    text-align: center;
+}
+.label-title {
+    background-color: white;
+}
+</style>
