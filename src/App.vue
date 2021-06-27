0df88f79f38aacd708c003f458efdf602027f4a2
Andrii Bikliann <biklyan13@gmail.com>
2021-06-27 18:35:56 +0300

    day commit

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,25 @@
 <template>
   <div id="app">
     <label-comp/>
-    <age-solver :user-age="50"/>
-    <product-table
-     :user-money="5000"
-     :products="dataProductsList"/>
   </div>
 </template>
 
 <script>
+
 import LabelComp from './components/LabelComp.vue'
-import AgeSolver from './components/AgeSolver.vue'
-import ProductTable from './components/ProductTable.vue'
 
 export default {
   name: 'App',
   components: {
-    LabelComp,
-    AgeSolver,
-    ProductTable
+    LabelComp
   },
     data() {
     return {
-      dataProductsList: [
-       {
-         id:1,
-         title:'TV',
-         price:2000
-       },
-       {
-         id:2,
-         title:'Fridge',
-         price:12000
-       },
-       {
-         id:3,
-         title:'TV2',
-         price:34000
-       },
-       {
-         id:4,
-         title:'Fridge2',
-         price:10000
-       },
-      ]
+      
     }
   },
 }
 </script>
-
-
-
-
-
-
-
-
 
 <style>
 #app {
